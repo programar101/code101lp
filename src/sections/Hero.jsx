8100466e7ee094ab78ecb3ec44891836cedc2b03
thehighestprimenumber <maru.c50@gmail.com>
2021-12-21 14:27:47 -0300

    close #3

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import heroImg from './heroImg.png';
+// eslint-disable-next-line
+import base from './../base.css';
+// eslint-disable-next-line
+import theme from './hero.css';
+
+export default function Hero(props) {
+    return (
+        <div className="container" >
+            <div className="text">
+                <h1>WORKSHOP</h1>
+                <h2>FULLSTACK DEVELOPER</h2>
+            </div>
+            <img className="image" src={heroImg} alt={"Fullstack Developer Bootcamp"}/>
+        </div>
+    );
+}
