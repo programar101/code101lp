fd93daef502afd68792154082f00939294118131
Christian Carballo <carballo.chr@gmail.com>
2021-12-30 18:06:36 -0300

    Tildes #5

--- src/Resumen/Resumen.jsx
+++ src/Resumen/Resumen.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import './resumen.css';
 
+
 export default function Resumen(props) {
     return (
         <div className="resumen" >
             <div>ENCUENTROS ONLINE</div>
-            <div>APRENDÉ ENSEÑANDO</div>
-            <div>INGLÉS INTERMEDIO (excluyente)</div>
-            <div>DEDICACIÓN SEMANAL: 5/10 hs</div>
+            <div>APREND&Eacute; ENSE&Ntilde;ANDO</div>
+            <div>INGL&Eacute;S INTERMEDIO (excluyente)</div>
+            <div>DEDICACI&Oacute;N SEMANAL: 5/10 hs</div>
         </div>
     );
 }
