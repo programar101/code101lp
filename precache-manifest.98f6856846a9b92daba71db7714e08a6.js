88e8c71a3850805d962c5a3e9ad6da2d1eb9cad4
thehighestprimenumber <maru.c50@gmail.com>
2021-12-31 17:58:43 -0300

    Updates

--- precache-manifest.98f6856846a9b92daba71db7714e08a6.js
+++ precache-manifest.98f6856846a9b92daba71db7714e08a6.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/code101lp/static/js/runtime~main.fb374f1f.js"
   },
   {
-    "revision": "728170b0c8a8c45917cd",
-    "url": "/code101lp/static/js/main.901ce228.chunk.js"
+    "revision": "fed0838da5f944944aca",
+    "url": "/code101lp/static/js/main.c414adab.chunk.js"
   },
   {
     "revision": "c658238e30aabcb6c879",
     "url": "/code101lp/static/js/2.abe721e7.chunk.js"
   },
   {
-    "revision": "728170b0c8a8c45917cd",
+    "revision": "fed0838da5f944944aca",
     "url": "/code101lp/static/css/main.32f06c87.chunk.css"
   },
   {
-    "revision": "eaaaa89ada8fb5297e3cd717cdc68fe0",
+    "revision": "0196f4380e72f7ee2b898e7272de4cae",
     "url": "/code101lp/index.html"
   }
 ];
