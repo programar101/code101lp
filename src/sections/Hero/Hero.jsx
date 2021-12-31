bd51a1654a5e076d50d06aa9dd24d282629602e6
thehighestprimenumber <maru.c50@gmail.com>
2021-12-31 18:14:46 -0300

    add CTA to hero #3

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -4,13 +4,19 @@ import heroImg from './heroImg.png';
 import base from './../../base.css';
 // eslint-disable-next-line
 import theme from './hero.css';
+import {Button} from "@mui/material";
 
 export default function Hero() {
     return (
-        <div className="heroContainer" >
-            <div className="text">
-                <h1>WORKSHOP</h1>
-                <h2>FULLSTACK DEVELOPER</h2>
+        <div className="heroContainer">
+            <div className="leftContainer">
+                <div className="text">
+                    <h1>WORKSHOP</h1>
+                    <h2>FULLSTACK DEVELOPER</h2>
+                </div>
+                <a href={"https://forms.gle/iptgMk59ePRmUPhn7"} className={"ctaButton"}>
+                    <h2>Inscripción</h2>
+                </a>
             </div>
             <img className="image" src={heroImg} alt={"Fullstack Developer Bootcamp"}/>
         </div>
