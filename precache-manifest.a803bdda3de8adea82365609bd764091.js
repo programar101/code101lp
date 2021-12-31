5d3109875ccc1a137a1ec18fb08681ebc2655be2
thehighestprimenumber <maru.c50@gmail.com>
2021-12-31 17:39:58 -0300

    Updates

---
+++ precache-manifest.a803bdda3de8adea82365609bd764091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3808072d49628e1984fda5d832f557d6",
+    "url": "/code101lp/static/media/heroImg.3808072d.png"
+  },
+  {
+    "revision": "038a9fc3b03c00b45c75",
+    "url": "/code101lp/static/js/runtime~main.fb374f1f.js"
+  },
+  {
+    "revision": "728170b0c8a8c45917cd",
+    "url": "/code101lp/static/js/main.901ce228.chunk.js"
+  },
+  {
+    "revision": "c658238e30aabcb6c879",
+    "url": "/code101lp/static/js/2.abe721e7.chunk.js"
+  },
+  {
+    "revision": "728170b0c8a8c45917cd",
+    "url": "/code101lp/static/css/main.32f06c87.chunk.css"
+  },
+  {
+    "revision": "eaaaa89ada8fb5297e3cd717cdc68fe0",
+    "url": "/code101lp/index.html"
+  }
+];
